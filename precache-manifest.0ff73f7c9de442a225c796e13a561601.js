6de6f495ff99890ce722f56a36624148d8c503cc
babilonczyk <piotrzkowski.jan@gmail.com>
2020-08-09 19:32:03 +0200

    Updates

---
+++ precache-manifest.0ff73f7c9de442a225c796e13a561601.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914917ba1aec71f48c84ef36a37d5067",
+    "url": "/babilonczyk/index.html"
+  },
+  {
+    "revision": "a1f334b0204a21b9ce8d",
+    "url": "/babilonczyk/static/css/main.089dc309.chunk.css"
+  },
+  {
+    "revision": "f694cc3e7044b2b546bb",
+    "url": "/babilonczyk/static/js/2.79f03c94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/babilonczyk/static/js/2.79f03c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f334b0204a21b9ce8d",
+    "url": "/babilonczyk/static/js/main.1ce1876f.chunk.js"
+  },
+  {
+    "revision": "5b0fee18304de7ba430e",
+    "url": "/babilonczyk/static/js/runtime-main.dee29128.js"
+  },
+  {
+    "revision": "ae541b8bf201dcac450db43e165a2acd",
+    "url": "/babilonczyk/static/media/css3.ae541b8b.svg"
+  },
+  {
+    "revision": "b1464b1c18a4e880c6dcf8bb00b73a2b",
+    "url": "/babilonczyk/static/media/enzyme.b1464b1c.png"
+  },
+  {
+    "revision": "178162791bff249ec35b7560f30746ec",
+    "url": "/babilonczyk/static/media/github.17816279.svg"
+  },
+  {
+    "revision": "e148643a1c2cea004bd9d9377b8c7542",
+    "url": "/babilonczyk/static/media/html.e148643a.svg"
+  },
+  {
+    "revision": "83b91b2989158968146b044eef51e3eb",
+    "url": "/babilonczyk/static/media/img1.83b91b29.jpg"
+  },
+  {
+    "revision": "dcb66079dc6bb52d3668a48ca9b67a2a",
+    "url": "/babilonczyk/static/media/jest.dcb66079.svg"
+  },
+  {
+    "revision": "738ab3c60cf8d346046c0875ad72651b",
+    "url": "/babilonczyk/static/media/js.738ab3c6.svg"
+  },
+  {
+    "revision": "b18f7f7f10a0bf632413622e75223232",
+    "url": "/babilonczyk/static/media/portfolio.b18f7f7f.png"
+  },
+  {
+    "revision": "1f38fb74f8c5d51504272e90a5cbe20a",
+    "url": "/babilonczyk/static/media/postgresql.1f38fb74.svg"
+  },
+  {
+    "revision": "a65aa8fc035cc5581018a342f31b40d7",
+    "url": "/babilonczyk/static/media/react-native.a65aa8fc.svg"
+  },
+  {
+    "revision": "e8d0a030c100db16afc056d4be520aa6",
+    "url": "/babilonczyk/static/media/redux.e8d0a030.svg"
+  },
+  {
+    "revision": "110c700b3c4b02286cbfa3b700af8a57",
+    "url": "/babilonczyk/static/media/ror.110c700b.png"
+  },
+  {
+    "revision": "89f02037518f62214935301f15403c85",
+    "url": "/babilonczyk/static/media/rpsec.89f02037.png"
+  },
+  {
+    "revision": "52ced50d8ecefb5c9e73fe3b928dcd4d",
+    "url": "/babilonczyk/static/media/ruby.52ced50d.svg"
+  },
+  {
+    "revision": "5f3d843eab48c686f277c220d0cad31d",
+    "url": "/babilonczyk/static/media/sass.5f3d843e.svg"
+  },
+  {
+    "revision": "bfa1254a6500dac86e4b82ae2f1775d4",
+    "url": "/babilonczyk/static/media/sinon.bfa1254a.png"
+  },
+  {
+    "revision": "dd063e143f694c6d502a02bc54ffdde1",
+    "url": "/babilonczyk/static/media/tiptopsite.dd063e14.png"
+  }
+]);
